3fc70623b14073a1975269ad43cd02ae7ffda59d
johnstark00000000-a11y <johnstark00000000@gmail.com>
2026-09-07 11:17:33 +0530

    Brain2 clone/admin.py

--- clone/admin.py
+++ clone/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import NeuralClone
-@admin.register(NeuralClone)
-class NeuralCloneAdmin(admin.ModelAdmin):
-    list_display = ("subject_name", "alias", "architecture", "status", "fidelity", "created_at")
+from .models import BrainProfile, ChatMessage, UploadItem
+admin.site.register(BrainProfile)
+admin.site.register(ChatMessage)
+admin.site.register(UploadItem)
