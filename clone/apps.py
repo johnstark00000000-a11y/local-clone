949787231228bafab06ff2fe7a44dfe9daaa86dc
johnstark00000000-a11y <johnstark00000000@gmail.com>
2026-09-07 11:17:31 +0530

    Brain2 clone/apps.py

--- clone/apps.py
+++ clone/apps.py
@@ -1,4 +1,4 @@
 from django.apps import AppConfig
 class CloneConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "clone"
+    default_auto_field='django.db.models.BigAutoField'
+    name='clone'
